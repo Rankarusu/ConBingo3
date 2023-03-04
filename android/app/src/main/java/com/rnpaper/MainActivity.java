3886cf93fc87c01ddc5f81880d04836d027062ed
Rankarusu <darkigfx@gmail.com>
2023-03-04 21:34:18 +0100

    chore: rename app

--- android/app/src/main/java/com/rnpaper/MainActivity.java
+++ android/app/src/main/java/com/rnpaper/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNPaper";
+    return "Convention Bingo";
   }
 
   @Override
