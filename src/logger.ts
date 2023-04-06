399e75e52ad28ec04b2777a2eb4eb4f76829de95
Rankarusu <darkigfx@gmail.com>
2023-04-06 11:40:33 +0200

    chore: add logs to stores

---
+++ src/logger.ts
@@ -0,0 +1,25 @@
+import {logger, consoleTransport} from 'react-native-logs';
+
+const config = {
+  severity: __DEV__ ? 'debug' : 'error',
+  transport: consoleTransport,
+  levels: {
+    debug: 0,
+    info: 1,
+    warn: 2,
+    error: 3,
+  },
+  transportOptions: {
+    colors: {
+      info: 'blueBright',
+      warn: 'yellowBright',
+      error: 'redBright',
+    },
+  },
+};
+
+type LogLevels = keyof typeof config.levels;
+
+const Logger = logger.createLogger<LogLevels>(config);
+
+export {Logger};
