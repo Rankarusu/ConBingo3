667c34513dc34e45c02d997b571861320063098c
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-06-14 23:26:53 +0200

    move stuff to app.tsx

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Text} from 'react-native-paper';
+import BingoField from '../components/BingoField';
+import BingoSheet from '../components/BingoSheet';
 
 const Play = () => {
-  return <Text>bleh</Text>;
+  return <BingoSheet />;
 };
 
 export default Play;
