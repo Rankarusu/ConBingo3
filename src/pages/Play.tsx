8527776e850c94c00a8932dab0ceb85937f120a1
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-06-14 23:27:05 +0200

    feat: add confetti on win, had to patch the package for center explosion

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,36 +1,75 @@
-import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {memo, RefObject, useEffect, useRef, useState} from 'react';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import ConfettiCannon from 'react-native-confetti-cannon';
 import {Button} from 'react-native-paper';
 import BingoSheet from '../components/BingoSheet';
 import {useAppDispatch, useAppSelector} from '../hooks';
 import {generateSheet} from '../hooks/useGenerateSheet';
-import {selectCurrentSheet, set} from '../stores/currentSheetSlice';
+import {
+  resetWin,
+  selectCurrentSheet,
+  selectWin,
+  set,
+} from '../stores/currentSheetSlice';
 import {reset, selectFields} from '../stores/fieldsSlice';
+
+interface ConfettiProps {
+  confettiRef: RefObject<ConfettiCannon>;
+}
+const {height, width} = Dimensions.get('window');
+const Confetti = memo((props: ConfettiProps) => (
+  <ConfettiCannon
+    autoStart={false}
+    count={200}
+    explosionSpeed={400}
+    fallSpeed={2000}
+    origin={{x: width / 2, y: height / 2}}
+    fadeOut={true}
+    autoStartDelay={0}
+    ref={props.confettiRef}
+  />
+));
 
 const Play = () => {
   const currentSheet = useAppSelector(selectCurrentSheet);
+  const win = useAppSelector(selectWin);
   const fields = useAppSelector(selectFields);
   const dispatch = useAppDispatch();
   const [isEditing, setIsEditing] = useState(false);
+
+  const confettiRef = useRef<ConfettiCannon>(null);
 
   const reroll = () => {
     const newSheet = generateSheet(fields);
     dispatch(set(newSheet));
   };
 
-  //idk if a useEffect si necessary here
+  const shootConfetti = () => {
+    confettiRef.current?.start();
+  };
+
+  //idk if a useEffect is necessary here
   if (fields.length < 24) {
     console.log('not enough fields, resetting');
+    //TODO: throw a warning here
     dispatch(reset());
   }
   if (!currentSheet || currentSheet.length !== 25) {
     console.log('generating new field, invalid data');
-    reroll();
+    reroll(); //TODO: on first launch out range in generateRandomNumbers is null
   }
+
+  useEffect(() => {
+    if (win) {
+      shootConfetti();
+      dispatch(resetWin());
+    }
+  }, [win, dispatch]);
 
   return (
     <View style={styles.wrapper}>
       <BingoSheet fields={currentSheet} />
+      <Confetti confettiRef={confettiRef} />
       <View style={styles.buttonBox}>
         <Button
           style={styles.button}
