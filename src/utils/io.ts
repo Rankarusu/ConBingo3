c1a82ede494a6b2aced266132e99956ca22aaaba
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-06-14 23:28:38 +0200

    fix: fix error handling on import. snackbar appeared when canceling

--- src/utils/io.ts
+++ src/utils/io.ts
@@ -2,6 +2,7 @@ import DocumentPicker from 'react-native-document-picker';
 import RNFS from 'react-native-fs';
 import Share from 'react-native-share';
 import {CheckableBingoField} from '../models/checkableBingoField';
+import {Logger} from './logger';
 
 const FILE_NAME = 'bingo-sheet.txt';
 const FILE_PATH = RNFS.CachesDirectoryPath + '/' + FILE_NAME;
@@ -16,26 +17,36 @@ export const share = async (sheet: CheckableBingoField[]) => {
   const sheetStr = JSON.stringify(sheet);
   await RNFS.writeFile(FILE_PATH, sheetStr, 'utf8');
 
-  console.log('saved');
-
   await Share.open({
     filename: FILE_NAME,
     url: FILE_URL,
     type: MIME_TYPE,
-  }).catch(error => console.log(error));
+  }).catch(error => Logger.warn(error));
 };
 
 export const load = async () => {
-  const file = await DocumentPicker.pickSingle({
-    type: MIME_TYPE,
-    mode: 'import',
-    presentationStyle: 'fullScreen',
-  });
+  let file;
+  try {
+    file = await DocumentPicker.pickSingle({
+      type: MIME_TYPE,
+      mode: 'import',
+      presentationStyle: 'fullScreen',
+    });
+  } catch (error) {
+    //this throws when use cancels the document picker
+    Logger.warn(error);
+  }
+
+  if (!file) {
+    return;
+  }
+
   const res = await RNFS.readFile(file.uri);
+
   if (!fieldsJsonRegex.test(res)) {
     throw new Error('File content is not a valid bingo sheet.');
   }
+
   const jsonField = JSON.parse(res);
-  console.log(jsonField);
   return jsonField as CheckableBingoField[];
 };
