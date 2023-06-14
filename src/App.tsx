eae915850cddabbf0081161af7fce9a41dcd0276
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-06-14 23:27:26 +0200

    refactor: make loading spinner use the entire screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
-import {ActivityIndicator, Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {PersistGate} from 'redux-persist/integration/react';
+import LoadingScreen from './components/LoadingScreen';
 import RootNavigation from './components/RootNavigation';
 import {AlertProvider} from './context/AlertContext';
 import {SnackbarProvider} from './context/SnackbarContext';
@@ -13,7 +14,7 @@ export default function App() {
   const theme = useAppSelector(selectAppTheme);
 
   return (
-    <PersistGate persistor={persistor} loading={<ActivityIndicator />}>
+    <PersistGate persistor={persistor} loading={<LoadingScreen />}>
       <NavigationContainer theme={theme}>
         <PaperProvider theme={theme}>
           <SnackbarProvider>
---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,22 @@
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {ActivityIndicator} from 'react-native-paper';
+
+const LoadingScreen = () => {
+  return (
+    <View style={styles.center}>
+      <ActivityIndicator size={'large'} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default LoadingScreen;
