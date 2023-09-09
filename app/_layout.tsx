e39075a7a1fb8176eaa580e063375aaecfda53af
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-09-09 11:21:02 +0200

    chore: remove console warnings and errors in web

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import {Slot, Stack} from 'expo-router';
 import {ErrorBoundaryProps} from 'expo-router/src/exports';
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
+import {Platform} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Provider as ReduxProvider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -20,6 +21,12 @@ export function ErrorBoundary(props: ErrorBoundaryProps) {
 }
 
 export default function Layout() {
+  // disable console warnings and errors in web so console is not spammed by deprecation warnings and stuff resulting from react-native-web
+  if (!__DEV__ && Platform.OS === 'web') {
+    console.warn = () => {};
+    console.error = () => {};
+  }
+
   return (
     //need to move redux provider to another component to we can query the theme inside the next
     <ReduxProvider store={store}>
