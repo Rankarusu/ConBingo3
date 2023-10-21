ff970df74173a152ce4a42cd01fd24e27fed6550
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-10-21 12:53:47 +0200

    chore(eslint): customize eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+    'import/ignore': ['react-native-reanimated'],
+  },
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'builtin',
+            position: 'before',
+          },
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+    'import/no-named-as-default': 'off',
+    radix: 'off',
+  },
 };
