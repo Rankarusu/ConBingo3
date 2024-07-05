de4b4c6157512b514c57751240a4b565e50cdb28
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2024-07-05 18:53:31 +0200

    chore(expo): upgrade expo to v51
    BREAKING CHANGE: framework update

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ module.exports = function (api) {
     plugins: [
       ['react-native-platform-specific-extensions', { extensions: ['css'] }],
       '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
     ],
     env: {
       production: {
